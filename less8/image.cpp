78315b1a9e6ba1b9be8b995af9671b3a5179dd0e
qnbhd <1qnbhd@gmail.com>
2019-03-12 18:00:03 +0300

    less 8

---
+++ less8/image.cpp
@@ -0,0 +1,32 @@
+#ifndef IMAGE
+#define IMAGE
+
+#include <string>
+#include <fstream>
+
+#include "xtl/xbase64.hpp"
+#include "nlohmann/json.hpp"
+
+namespace im
+{
+    struct image
+    {
+        inline image(const std::string& filename)
+        {
+            std::ifstream fin(filename, std::ios::binary);
+            m_buffer << fin.rdbuf();
+        }
+
+        std::stringstream m_buffer;
+    };
+
+    nlohmann::json mime_bundle_repr(const image& i)
+    {
+        auto bundle = nlohmann::json::object();
+        bundle["image/png"] = xtl::base64encode(i.m_buffer.str());
+        return bundle;
+    }
+}
+
+
+#endif 
